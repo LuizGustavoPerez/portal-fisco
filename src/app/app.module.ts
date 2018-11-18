e9f3244bd80764966da8460cc64c12e1ef31f9c3
Luiz Gustavo Perez <luizgustavo.perez@outlook.com>
2018-11-18 16:36:57 -0200

    Changed structure project, created components to home page and insert font awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SearchComponent } from './search/search.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchComponent } from './components/search/search.component';
+import { SupportComponent } from './components/support/support.component';
+import { HomeComponent } from './components/home/home.component';
+import { HelpComponent } from './components/help/help.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SearchComponent
+    SearchComponent,
+    SupportComponent,
+    HomeComponent,
+    HelpComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
