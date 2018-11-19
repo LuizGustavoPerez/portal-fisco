dd5fd3875d54c3c2f5fed90859dd6bbcde152c82
Luiz Gustavo Perez <luizgustavo.perez@outlook.com>
2018-11-19 21:53:45 -0200

    added some styles and effects to about page

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  type: String;
+  textType: String;
+  show: boolean;
+  
   constructor() { }
 
   ngOnInit() {
+    this.show = false;
+  }
+
+  viewDetail(type: String) {
+    if (this.show == false) {
+      this.show = true;
+    }
+    
+    this.type = type.toUpperCase();
+
+    if (type === "processo") {
+      this.textType = `Todos os processos implantados 
+        têm como essência o potencial crescimento da sua empresa, assim os mesmos, passam por 
+        cuidadosas avaliações e pelo crivo dos gestores da área.`
+    } else if (type === "operacional") {
+      this.textType = `Contamos com uma equipe operacional qualificada, 
+        movida por desafios e inovação. Manter-se atualizado é o nosso foco.`
+    } else if (type === "tecnologia") {
+      this.textType = `Soluções específicas e no tempo determinado. Contamos com especialistas 
+        na área de Tecnologia, hábeis e capacitados para atender demandas peculiares, dos diversos 
+        segmente existentes.`
+    } else if (type === "comercial") {
+      this.textType = `Excelência no atendimento e satisfação do cliente geram bons frutos. Assim, 
+        nosso objetivo é manter alto nível de qualidade e exclusividade dos serviços, simultâneo a 
+        construção e manutenção de relacionamento e agenda positiva.`
+    } else {
+      this.type = "Ops, texto não encontrado"
+      this.textType = "Desculpe, mas o texto não pode ser carregado"
+    }
+
   }
 
 }
